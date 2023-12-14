1af89466d0eb618eb7662142f31d4d7584ff2386
test <test@test.com>
2023-12-14 18:43:47 +0900

    lesson02-ex01

---
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -0,0 +1,17 @@
+package com.example.lesson02.bo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.lesson02.domain.UsedGoods;
+
+@Service // spring bean
+public class UsedGoodsBO {
+
+	// input:X (컨트롤러한테 받아온다.) 
+	// output: (Repository한테 받은 것을) List<UsedGoods> (컨트롤러한테 준다.)
+	public List<UsedGoods> getUsedGoodsList() {
+		return ;
+	}
+}
